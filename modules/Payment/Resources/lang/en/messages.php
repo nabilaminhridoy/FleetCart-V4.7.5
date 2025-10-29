55615c335d5b222b3c4e6f5295b3d284759b3508
Nabil Amin Hridoy <66374353+nabilaminhridoy@users.noreply.github.com>
2025-10-29 22:19:25 +0600

    Initial commit

--- modules/Payment/Resources/lang/en/messages.php
+++ modules/Payment/Resources/lang/en/messages.php
@@ -7,4 +7,7 @@ return [
     'currency_not_supported' => 'Currency not supported',
     'payment_gateway_error' => 'Payment gateway error!',
     'payment_cancelled' => 'Payment cancelled!',
+    'uddoktapay_error' => 'UddoktaPay payment error: :message',
+    'uddoktapay_verification_failed' => 'Payment verification failed',
+    'uddoktapay_webhook_error' => 'Webhook processing failed',
 ];
